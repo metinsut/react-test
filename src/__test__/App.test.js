d090984a89ef593606fe4919c1bd2643823eb726
metinsut <metindir@gmail.com>
2018-12-22 17:32:34 +0300

    edited

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../components/App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+	wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-	const div = document.createElement('div');
+	expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-	ReactDOM.render(<App />, div);
-
-	expect(div.innerHTML).toContain('Comment Box');
-
-	ReactDOM.unmountComponentAtNode(div);
+it('shows a comment list', () => {
+	expect(wrapped.find(CommentList).length).toEqual(1);
 });
