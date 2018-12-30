6f38753d94139c6463c9a5191969f72b8c629e66
metinsut <metindir@gmail.com>
2018-12-30 20:28:34 +0300

    updated

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-	return <div>Comment Box</div>;
-};
+class CommentBox extends Component {
+	state = {
+		comment: ''
+	};
+
+	handleChange = e => {
+		this.setState({ comment: e.target.value });
+	};
+
+	handleSubmit = e => {
+      // TODO
+		e.preventDefault();
+		this.setState({ comment: '' });
+	};
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<h4>Add a comment</h4>
+				<textarea value={this.state.comment} onChange={this.handleChange} cols="30" rows="5" />
+				<div>
+					<button>Submit Comment</button>
+				</div>
+			</form>
+		);
+	}
+}
+
+export default CommentBox;
